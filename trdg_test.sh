e7a4f0acefe6e78a4bbaf778be53f4fe3030eb49
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-03-27 21:37:10 +0530

    fonts

--- trdg_test.sh
+++ trdg_test.sh
@@ -1,47 +1,16 @@
 #! /bin/bash
 
-python trdg/run.py -l kr -dt trdg/dicts/kr.txt -fd trdg/fonts/new_kr --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/ -tc "#030303" -f 64 -w 10 -m 12,5,2,5 -c 10 --image_count 0 
-python trdg/run.py -l ur -dt trdg/dicts/urSpc.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 5 -sw 1 -stw 2 -m 12,5,2,5 -c 10 --image_count 10
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 5 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 20 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 5 -sw 1 -stw 2 -m 12,5,2,5 -c 10 --image_count 30
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 5 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 40 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 5 -sw 1 -stw 2 -m 12,5,2,5 -c 10 --image_count 50
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 0 -bl 1 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#030303" -f 90 -w 10 -c 200000 -sw 0.1 -stw 3   -m 10,1,1,1 --image_count 0
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 0 -bl 0 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#4B4B4B" -f 70 -w 8 -c 200000 -sw 0.1 -stw 2   -m 10,1,1,1 --image_count 200000
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 2 -bl 1 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#030303" -f 90 -w 10 -c 200000 -sw 0.1 -stw 3   -m 10,1,1,1 --image_count 400000
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 2 -bl 0 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#4B4B4B" -f 70 -w 8 -c 200000 -sw 0.1 -stw 2   -m 10,1,1,1 --image_count 600000
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 3 -bl 1 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#030303" -f 90 -w 10 -c 200000 -sw 0.1 -stw 3   -m 10,1,1,1 --image_count 800000
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 3 -bl 0 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#4B4B4B" -f 70 -w 8 -c 200000 -sw 0.1 -stw 2   -m 10,1,1,1 --image_count 1000000
 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 60 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 1 -m 12,5,2,5 -c 10 --image_count 70 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 80 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 1 -m 12,5,2,5 -c 10 --image_count 90 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 100 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 1 -m 12,5,2,5 -c 10 --image_count 110 
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 0 -bl 1 -rbl -d 2 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#030303" -f 90 -w 1 -c 200000 -sw 0.1 -stw 3   -m 10,1,1,1 --image_count 1200000
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 3 -bl 0 -rbl -d 2 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#4B4B4B" -f 70 -w 1 -c 200000 -sw 0.1 -stw 2   -m 10,1,1,1 --image_count 1400000
 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ab --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 10 -stw 2 -sw 0.5  -m 1,1,1,1 -c 10 --image_count 120 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ab --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 64 -w 12 -stw 1 -sw 0.3  -m 0,0,0,0 -c 10 --image_count 130 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ab --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -sw 0.5 -m 1,1,1,1 -c 10 --image_count 140
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ab --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 120 -w 12 -stw 1 -sw 0.3 -m 0,0,0,0 -c 10 --image_count 150 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ab --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -sw 0.5 -m 1,1,1,1 -c 10 --image_count 160
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ab --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 120 -w 12 -stw 1 -sw 0.3 -m 0,0,0,0 -c 10 --image_count 170 
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 3 -bl 2 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#030303" -f 90 -w 10 -c 200000 -sw 0.5 -stw 3   -m 10,5,5,5 --image_count 1600000
+python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur_selected --word_split -na 2 -t 55 -k 3 -rk -b 3 -bl 2 -rbl -d 5 -fi --output_dir /DATA/Tawheed/new_trdg_train/ -tc "#4B4B4B" -f 70 -w 8 -c 200000 -sw 0.5 -stw 2   -m 10,5,5,5 --image_count 1800000
 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 2 -m 5,650,5,5 -c 10 --image_count 180 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -w 2 -m 5,650,5,5 -c 10 --image_count 190 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 120 -w 3 -m 5,650,5,5 -c 10 --image_count 200
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 120 -w 3 -m 5,650,5,5 -c 10 --image_count 210
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 64 -w 2 -m 5,650,5,5 -c 10 --image_count 220 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 3 -m 5,650,5,5 -c 10 --image_count 230
-
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 0.2 -m 12,5,2,5 -c 10 --image_count 240 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 8 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 250
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 0.2 -m 12,5,2,5 -c 10 --image_count 260 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 8 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 270
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -sw 0.2 -m 12,5,2,5 -c 10 --image_count 280 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 8 -sw 0.5 -m 12,5,2,5 -c 10 --image_count 290
-
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -m 5,5,5,5 -c 10 --image_count 300 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -m 5,5,5,5 -c 10 --image_count 310
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -m 5,5,5,5 -c 10 --image_count 320 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 2 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -m 5,5,5,5 -c 10 --image_count 330
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -m 5,5,5,5 -c 10 --image_count 340 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 120 -w 10 -stw 2 -m 5,5,5,5 -c 10 --image_count 350
-
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 0 -bl 0 --output_dir trdg/output/urdu_train_12M -tc "#4B4B4B" -f 64 -stw 2 -m 5,5,5,5 -c 10 --image_count 360 
-python trdg/run.py -l ur -dt trdg/dicts/urdu.txt -fd trdg/fonts/ur --word_split -na 2 -t 55 -k 5 -rk -b 3 -bl 1 --output_dir trdg/output/urdu_train_12M -tc "#030303" -f 64 -stw 2 -m 5,5,5,5 -c 10 --image_count 370
 
